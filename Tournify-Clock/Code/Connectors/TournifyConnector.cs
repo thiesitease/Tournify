4b185df6a0858b835bbfc74b692af6e80b59700a
Thies Reinhold <65960018+thiesitease@users.noreply.github.com>
2025-06-17 23:18:55 +0200

    Auslesen Tournify über webview2 control

---
+++ Tournify-Clock/Code/Connectors/TournifyConnector.cs
@@ -0,0 +1,126 @@
+﻿using HtmlAgilityPack;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Diagnostics;
+
+
+namespace Gemelo.Applications.Tournify.Clock.Code.Connectors;
+public class TournifyConnector
+{
+    #region öffentliche Properties
+
+    public Uri TournifyUri { get; }
+
+    #endregion öffentliche Properties
+
+    #region ctor
+
+    public TournifyConnector(Uri tournifyUri)
+    {
+        TournifyUri = tournifyUri;
+    }
+
+
+    #endregion ctor
+
+    #region öffentliche Methoden
+
+    //public async Task ReadContent(string html)
+    //{
+    //    try
+    //    {
+          
+
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //    }
+    //}
+
+
+    public async Task ReadContent(string html)
+    {
+        try
+        {
+            //var wc = new HttpClient();
+            //string webData = await wc.GetStringAsync(TournifyUri);
+
+            //var url = "https://example.com";  // Ersetze mit gewünschter URL
+
+            //using var httpClient = new HttpClient();
+            //var html = await httpClient.GetStringAsync(TournifyUri);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            Parse(htmlDoc.DocumentNode);
+
+            var linkNodes = htmlDoc.DocumentNode.SelectNodes("//a[@href]");
+
+            if (linkNodes != null)
+            {
+                foreach (var link in linkNodes)
+                {
+                    var href = link.GetAttributeValue("href", string.Empty);
+                    var text = link.InnerText.Trim();
+
+                    Console.WriteLine($"Text: {text} | Link: {href}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Keine Links gefunden.");
+            }
+
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+
+    private void Parse(HtmlNode parentNode)
+    {
+        if (parentNode.Name == "div")
+        {
+            //Debugger.Break();
+            foreach (var attrib in parentNode.Attributes)
+            {
+                Debug.WriteLine(attrib.Name);
+                Debug.WriteLine(attrib.Value);
+
+                //if (attrib.Name == "class" )
+                //{
+                //    Debugger.Break();
+                //}
+
+
+                if (attrib.Name == "class" && 
+                    attrib.Value == "MatchRow Columns6")
+                {
+                    Debugger.Break();
+                }
+            }
+
+        }
+
+        foreach (var node in parentNode.ChildNodes)
+        {
+            Parse(node);
+        }
+    }
+
+
+
+
+    #endregion öffentliche Methoden
+}
---
+++ Tournify-Clock/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Gemelo.Applications.Tournify.Clock.Code.Connectors;
+using Gemelo.Applications.Tournify.Clock.Properties;
+
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tournify_Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Member and Properties
+
+        public TournifyConnector Connector { get; } = new TournifyConnector(
+            new Uri(Settings.Default.TournifyUrl));
+
+        #endregion Member and Properties
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            m_Web.Loaded += Web_Loaded;
+        }
+
+        private void Web_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        #region EventHandler
+
+        private void ButtonRead_Click(object sender, RoutedEventArgs e)
+        {
+            _ = Parse();
+        }
+
+        private async Task Parse()
+        {
+            string html = await DumpHtml();
+            await Connector.ReadContent(html);
+        }
+
+        #endregion EventHandler
+
+        private void ButtonOpenWeb_Click(object sender, RoutedEventArgs e)
+        {
+            _ = Open();
+        }
+
+        private async Task Open()
+        {
+            await m_Web.EnsureCoreWebView2Async(null);
+            m_Web.Source = new(Settings.Default.TournifyUrl);
+        }
+
+        private async Task<string> DumpHtml()
+        {
+            string html = await m_Web.ExecuteScriptAsync("document.documentElement.outerHTML");
+
+            // WebView2 gibt einen JSON-String zurück (mit Anführungszeichen)
+            string decodedHtml = System.Text.Json.JsonSerializer.Deserialize<string>(html);
+
+            // Jetzt kannst du den HTML-Quelltext verwenden
+            Console.WriteLine(decodedHtml);
+
+            return decodedHtml;
+        }
+    }
+}
